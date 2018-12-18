c5176d8c8b8a0dc8aadcd1cfc5ed67cf9f31ec10
873421427 <873421427@qq.com>
2018-12-18 21:11:52 +0800

    ,

--- service/notFound.go
+++ service/notFound.go
@@ -3,10 +3,19 @@ package service
 import(
 	"encoding/json"
 	"net/http"
+	"fmt"
 )
 
 func NotFound(w http.ResponseWriter, req * http.Request){
+	vals := req.URL.Query()
+	callbackName := vals.Get("callback")
+	if callbackName == ""{
+		fmt.Fprintf(w,"Please give a callback name in query string.")
+		return 
+	}
+
 	response := map[string]string{"detail":"Not found"}
 	rm,_ := json.Marshal(response)
-	w.Write(rm)
+	//w.Write(rm)
+	fmt.Fprintf(w,"%s(%s)",callbackName,rm)
 }
